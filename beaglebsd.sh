e13c44825564f0938a96f9ce87e7148fcd31d8e2
Tim Kientzle <kientzle@acm.org>
2012-05-06 11:30:45 -0700

    Separate the progress messages for installkernel and installworld.

--- beaglebsd.sh
+++ beaglebsd.sh
@@ -172,9 +172,10 @@ cp files/ubldr ${BUILDOBJ}/_.mounted_fat/
 #
 # Install FreeBSD kernel and world onto UFS partition.
 #
-echo "Installing FreeBSD onto the UFS partition at "`date`
 cd $FREEBSD_SRC
+echo "Installing FreeBSD kernel onto the UFS partition at "`date`
 make TARGET_ARCH=arm TARGET_CPUTYPE=armv6 DESTDIR=${BUILDOBJ}/_.mounted_ufs KERNCONF=${KERNCONF} installkernel > ${BUILDOBJ}/_.installkernel.log 2>&1
+echo "Installing FreeBSD world onto the UFS partition at "`date`
 make TARGET_ARCH=arm TARGET_CPUTYPE=armv6 DESTDIR=${BUILDOBJ}/_.mounted_ufs installworld > ${BUILDOBJ}/_.installworld.log 2>&1
 make TARGET_ARCH=arm TARGET_CPUTYPE=armv6 DESTDIR=${BUILDOBJ}/_.mounted_ufs distrib-dirs > ${BUILDOBJ}/_.distrib-dirs.log 2>&1
 make TARGET_ARCH=arm TARGET_CPUTYPE=armv6 DESTDIR=${BUILDOBJ}/_.mounted_ufs distribution > ${BUILDOBJ}/_.distribution.log 2>&1
