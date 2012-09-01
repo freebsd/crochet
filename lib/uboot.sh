c18c46e56c32ee74510908a5a7ee2d434fee1461
Tim Kientzle <kientzle@acm.org>
2012-09-01 11:22:49 -0700

    Several more improvements:    * Record patches applied when patching U-boot.    * Record configuration used when configuring U-Boot.    * Check gmake during prerequisite check    * Comment most of the functions.

--- lib/uboot.sh
+++ lib/uboot.sh
@@ -24,6 +24,12 @@ _uboot_test ( ) {
 	_uboot_download_instructions "$2"
 	exit 1
     fi
+    if [ -z `which gmake` ]; then
+	echo "U-Boot build requires 'gmake'"
+	echo "Please install and re-run this script."
+	exit 1
+    fi
+
     echo "Found suitable U-Boot sources in $UBOOT_SRC"
 }
 
@@ -37,37 +43,53 @@ uboot_ti_test ( ) {
 	"git://arago-project.org/git/projects/u-boot-am33x.git"
 }
 
+# uboot_patch: Apply patches to the U-Boot sources.
+#
+# $@: List of patch files to apply
 uboot_patch ( ) {
-    if [ ! -f ${UBOOT_SRC}/_.uboot.patched ] && [ ! -f ${WORKDIR}/_.uboot.patched ]; then
-	cd "$UBOOT_SRC"
-	echo "Patching U-Boot. (Logging to ${WORKDIR}/_.uboot.patch.log)"
-	for p in "$@"; do
-	    echo "   Applying patch $p"
-	    patch -N -p1 < $p >> ${WORKDIR}/_.uboot.patch.log 2>&1
-	done
-
-	rm -f ${WORKDIR}/_.uboot.configured
+    # TODO: Verify that _.uboot.patched lists the patch files
+    # we expect.  If not, complain and exit.
+    if [ ! -f ${UBOOT_SRC}/_.uboot.patched ]; then
+	return 0
     fi
-    touch ${UBOOT_SRC}/_.uboot.patched
+
+    if [ ! -f ${WORKDIR}/_.uboot.patched ]; then
+	touch ${UBOOT_SRC}/_.uboot.patched
+	return 0
+    fi
+
+    cd "$UBOOT_SRC"
+    echo "Patching U-Boot. (Logging to ${WORKDIR}/_.uboot.patch.log)"
+    for p in "$@"; do
+	echo "   Applying patch $p"
+	patch -N -p1 < $p >> ${WORKDIR}/_.uboot.patch.log 2>&1
+    done
+    echo "$@" > ${UBOOT_SRC}/_.uboot.patched
+    rm -f ${WORKDIR}/_.uboot.configured
 }
 
+# uboot_configure
+#
+# $1: Name of U-Boot configuration to use.
 uboot_configure ( ) {
     if [ ! -f ${WORKDIR}/_.uboot.configured ]; then
 	cd "$UBOOT_SRC"
 	echo "Configuring U-Boot. (Logging to ${WORKDIR}/_.uboot.configure.log)"
 	gmake CROSS_COMPILE=arm-freebsd- $1 > ${WORKDIR}/_.uboot.configure.log 2>&1
-	touch ${WORKDIR}/_.uboot.configured
+	echo "$1" > ${WORKDIR}/_.uboot.configured
 	rm -f ${WORKDIR}/_.uboot.built
     fi
 }
 
+# uboot_build
+#
 uboot_build ( ) {
-    if [ ! -f ${WORKDIR}/_.uboot.built ]; then
-	cd "$UBOOT_SRC"
-	echo "Building U-Boot. (Logging to ${WORKDIR}/_.uboot.build.log)"
-	gmake CROSS_COMPILE=arm-freebsd- > ${WORKDIR}/_.uboot.build.log 2>&1
-	touch ${WORKDIR}/_.uboot.built
-    else
+    if [ -f ${WORKDIR}/_.uboot.built ]; then
 	echo "Using U-Boot from previous build."
     fi
+
+    cd "$UBOOT_SRC"
+    echo "Building U-Boot. (Logging to ${WORKDIR}/_.uboot.build.log)"
+    gmake CROSS_COMPILE=arm-freebsd- > ${WORKDIR}/_.uboot.build.log 2>&1
+    touch ${WORKDIR}/_.uboot.built
 }
