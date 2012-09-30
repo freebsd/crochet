c707c713f10bdcecc699f0aa1b36a4a727ab7c72
Tim Kientzle <kientzle@acm.org>
2012-09-29 23:06:17 -0700

    Use my fork of the Arago sources instead of using them directly. In particular, this should give me (almost) the best of both worlds:   * Up-to-date BeagleBone support code   * Stable sources that won't keep me running to manage patches

--- board/BeagleBone/setup.sh
+++ board/BeagleBone/setup.sh
@@ -1,25 +1,31 @@
 KERNCONF=BEAGLEBONE
-UBOOT_SRC=${TOPDIR}/u-boot-2012.07
+UBOOT_SRC=${TOPDIR}/u-boot-beaglebone-freebsd
 
 board_check_prerequisites ( ) {
     freebsd_current_test
 
-    # I used to use the TI Arago sources, but those change
-    # so quickly that I got tired of chasing patches.  If you
-    # want to try them:
+    # I used to use the Arago project sources directly, but those
+    # change quickly and I got tired of chasing patches.  If you want
+    # to try them:
     #
-    # "git clone git://arago-project.org/git/projects/u-boot-am33x.git ${UBOOT_SRC}"
+    #uboot_test \
+    #   "$UBOOT_SRC/board/ti/am335x/Makefile" \
+    #   "git clone git://arago-project.org/git/projects/u-boot-am33x.git ${UBOOT_SRC}"
 
+    # I now use a fork of the Arago sources with FreeBSD-specific patches
+    # pre-applied:
     uboot_test \
 	"$UBOOT_SRC/board/ti/am335x/Makefile" \
-	"fetch ftp://ftp.denx.de/pub/u-boot/u-boot-2012.07.tar.bz2" \
-	"tar xf u-boot-2012.07.tar.bz2"
+	"git clone https://github.com/kientzle/u-boot-beaglebone-freebsd.git ${UBOOT_SRC}"
 }
 
 board_build_bootloader ( ) {
     freebsd_ubldr_build UBLDR_LOADADDR=0x88000000
 
-    uboot_patch ${BOARDDIR}/files/uboot_*.patch
+    # One advantage of maintaining a clone of the Arago
+    # sources:  I can just keep changes there instead
+    # of managing patches.
+    #uboot_patch ${BOARDDIR}/files/uboot_*.patch
     uboot_configure am335x_evm_config
     uboot_build
 }
