3cbd897ff3f883f7cb516f57c416ceddbe790a25
Tim Kientzle <kientzle@acm.org>
2012-12-08 10:15:28 -0800

    First cut at VideoCore building for RPi. Since this is RPi-specific, I'm putting it in the board directory instead of the general lib directory.

---
+++ board/RaspberryPi/videocore.sh
@@ -0,0 +1,81 @@
+
+videocore_src_check ( ) {
+    if [ ! -d $VC_SRC ]; then
+	echo "Need VideoCore kernel module code for FreeBSD"
+	echo "Use the following command to fetch them:"
+	echo
+	echo " $ git clone git://github.com/gonzoua/vchiq-freebsd.git ${VC_SRC}"
+	echo
+	echo "Run this script again after you have the files."
+	exit 1
+    fi
+}
+
+videocore_build ( ) (
+    if [ -f ${WORKDIR}/_.built-videocore-module ]
+    then
+	echo "Using VideoCore kernel module from previous build"
+	return 0;
+    fi
+
+    echo "Building VideoCore kernel module"
+    cd ${FREEBSD_SRC}
+    buildenv=`make TARGET_ARCH=$TARGET_ARCH buildenvvars`
+    eval $buildenv SYSDIR=${FREEBSD_SRC}/sys MAKESYSPATH=${FREEBSD_SRC}/share/mk make -C ${VC_SRC}
+)
+
+# $1: Target directory for install
+videocore_install ( ) (
+    echo "Installing VideoCore kernel module"
+    cd ${FREEBSD_SRC}
+    buildenv=`make TARGET_ARCH=$TARGET_ARCH buildenvvars`
+    eval $buildenv SYSDIR=${FREEBSD_SRC}/sys MAKESYSPATH=${FREEBSD_SRC}/share/mk make -C ${VC_SRC} DESTDIR=$1 install
+)
+
+CMAKE=`which cmake`
+cmake_check ( ) {
+    if [ -z ${CMAKE} ]; then
+	echo "VideoCore userland build requires 'cmake'"
+	echo "Please install and re-run this script."
+	exit 1
+    fi
+}
+
+videocore_user_check ( ) {
+    cmake_check
+    if [ ! -d $VC_USER_SRC ]; then
+	echo "Need VideoCore user library code for FreeBSD"
+	echo "Use the following command to fetch them:"
+	echo
+	echo " $ git clone -b freebsd git://github.com/gonzoua/userland.git ${VC_USER_SRC}"
+	echo
+	echo "Run this script again after you have the files."
+	exit 1
+    fi
+}
+
+videocore_user_build ( ) (
+    if [ -f ${WORKDIR}/_.built-videocore-library ]
+    then
+	echo "Using VideoCore user library from previous build"
+	return 0;
+    fi
+
+    echo "Building VideoCore user library"
+    cd ${FREEBSD_SRC}
+    buildenv=`make TARGET_ARCH=$TARGET_ARCH buildenvvars`
+    _VC_BUILDDIR=${VC_USER_SRC}/build/arm-freebsd/release/
+    mkdir -p ${_VC_BUILDDIR}
+    cd ${_VC_BUILDDIR}
+    # Should the toolchain file be in the board directory?
+    # Would that let us use the upstream videocore directly?
+    eval $buildenv $CMAKE -DCMAKE_TOOLCHAIN_FILE=${VC_USER_SRC}/makefiles/cmake/toolchains/arm-freebsd.cmake -DCMAKE_BUILD_TYPE=Release ${VC_USER_SRC}
+    cd ${_VC_BUILDDIR}
+    eval $buildenv make
+)
+
+# $1: DESTDIR
+videocore_user_install ( ) (
+    echo "TODO: Install videocore library"
+    exit 1
+)
