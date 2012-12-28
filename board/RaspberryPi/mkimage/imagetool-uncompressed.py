6810e9cad2fb29a5d5a2f22cf749e5586c28d2aa
Tim Kientzle <kientzle@acm.org>
2012-12-27 22:16:00 -0800

    Make imagetool-uncompressed.py a little more useful. In particular, make it support writing the generated result to stdout or to a specified path.

--- board/RaspberryPi/mkimage/imagetool-uncompressed.py
+++ board/RaspberryPi/mkimage/imagetool-uncompressed.py
@@ -4,43 +4,61 @@ import os
 import re
 import sys
 
+# Template and parameters are stored in same directory
+# as this Python file
+PATH=os.path.dirname(__file__)
+
 try:
    input_image = sys.argv[1]
    output_image = sys.argv[2]
 except:
    input_image = ""
 
+# Open input (which may be stdin)
 if input_image == "":
-  print("usage : imagetool-uncompressed.py <input image> <output image>");
+  sys.stderr.write("usage : imagetool-uncompressed.py <input image> <output image>\n");
   sys.exit(0)
-   
-re_line = re.compile(r"0x(?P<value>[0-9a-f]{8})")
+elif not input_image or input_image == '-':
+   infile = sys.stdin
+else:
+   infile = open(input_image, "rb")
 
-mem = [0 for i in range(32768)]
+# Open output (which may be stdout)
+if not output_image or output_image == '-':
+   outfile = sys.stdout
+else:
+   outfile = open(output_image, "wb")
 
+# Read in template for first 32k   
+f = open(os.path.join(PATH, "first32k.bin"), "rb")
+mem = [c for c in f.read(32768)]
+f.close()
+
+# Overlay boot parameters and args onto template
 def load_to_mem(name, addr):
-   f = open(name)
+   f = open(os.path.join(PATH, name))
 
    for l in f.readlines():
-      m = re_line.match(l)
-
-      if m:
-         value = int(m.group("value"), 16)
-
+      if l.startswith('0x'):
+         value = int(l, 0)
          for i in range(4):
-            mem[addr] = int(value >> i * 8 & 0xff)
+            mem[addr] = chr(int(value >> i * 8 & 0xff))
             addr += 1
 
    f.close()
-
 load_to_mem("boot-uncompressed.txt", 0x00000000)
 load_to_mem("args-uncompressed.txt", 0x00000100)
 
-f = open("first32k.bin", "wb")
+# Write out header
+outfile.write("".join(mem))
 
-for m in mem:
-   f.write(chr(m))
+# Copy input image after header
+while True:
+   piece = infile.read(4096)
+   if piece == "":
+      break # end of file
+   outfile.write(piece)
 
-f.close()
-
-os.system("cat first32k.bin " + input_image + " > " + output_image)
+# Close files
+infile.close()
+outfile.close()
