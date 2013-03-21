a35334d334fd056b92be036b466d92652b52719b
Tim Kientzle <kientzle@acm.org>
2013-03-20 22:38:03 -0700

    Make videocore build a little more robust: exit on failure, clear build marker if source is not present

--- board/RaspberryPi/videocore.sh
+++ board/RaspberryPi/videocore.sh
@@ -9,11 +9,12 @@ videocore_src_check ( ) {
 	echo " $ git clone git://github.com/gonzoua/vchiq-freebsd.git ${RPI_VC_SRC}"
 	echo
 	echo "Run this script again after you have the files."
+	rm ${WORKDIR}/_.built-videocore-module
 	exit 1
     fi
 }
 
-videocore_build ( ) (
+videocore_build ( ) {
     if [ -f ${WORKDIR}/_.built-videocore-module ]
     then
 	echo "Using VideoCore kernel module from previous build"
@@ -23,18 +24,19 @@ videocore_build ( ) (
     echo "Building VideoCore kernel module"
     cd ${FREEBSD_SRC}
     buildenv=`make TARGET_ARCH=$TARGET_ARCH buildenvvars`
-    eval $buildenv SYSDIR=${FREEBSD_SRC}/sys MAKESYSPATH=${FREEBSD_SRC}/share/mk make -C ${RPI_VC_SRC}
+    echo "buildenv: $buildenv"
+    eval $buildenv SYSDIR=${FREEBSD_SRC}/sys MAKESYSPATH=${FREEBSD_SRC}/share/mk make -C ${RPI_VC_SRC} || exit 1
 
     touch ${WORKDIR}/_.built-videocore-module
-)
+}
 
 # $1: Target directory for install
-videocore_install ( ) (
+videocore_install ( ) {
     echo "Installing VideoCore kernel module"
     cd ${FREEBSD_SRC}
     buildenv=`make TARGET_ARCH=$TARGET_ARCH buildenvvars`
-    eval $buildenv SYSDIR=${FREEBSD_SRC}/sys MAKESYSPATH=${FREEBSD_SRC}/share/mk make -C ${RPI_VC_SRC} DESTDIR=$1 install
-)
+    eval $buildenv SYSDIR=${FREEBSD_SRC}/sys MAKESYSPATH=${FREEBSD_SRC}/share/mk make -C ${RPI_VC_SRC} DESTDIR=$1 install || exit 1
+}
 
 CMAKE=`which cmake`
 cmake_check ( ) {
@@ -54,11 +56,12 @@ videocore_user_check ( ) {
 	echo " $ git clone -b freebsd git://github.com/gonzoua/userland.git ${RPI_VC_USER_SRC}"
 	echo
 	echo "Run this script again after you have the files."
+	rm ${WORKDIR}/_.built-videocore-library
 	exit 1
     fi
 }
 
-videocore_user_build ( ) (
+videocore_user_build ( ) {
     if [ -f ${WORKDIR}/_.built-videocore-library ]
     then
 	echo "Using VideoCore user library from previous build"
@@ -73,15 +76,14 @@ videocore_user_build ( ) (
     cd ${_VC_BUILDDIR}
     # Should the toolchain file be in the board directory?
     # Would that let us use the upstream videocore directly?
-    eval $buildenv $CMAKE -DCMAKE_TOOLCHAIN_FILE=${RPI_VC_USER_SRC}/makefiles/cmake/toolchains/arm-freebsd.cmake -DCMAKE_BUILD_TYPE=Release ${RPI_VC_USER_SRC}
+    eval $buildenv $CMAKE -DCMAKE_TOOLCHAIN_FILE=${RPI_VC_USER_SRC}/makefiles/cmake/toolchains/arm-freebsd.cmake -DCMAKE_BUILD_TYPE=Release ${RPI_VC_USER_SRC} || exit 1
     cd ${_VC_BUILDDIR}
-    eval $buildenv make
-
+    eval $buildenv make || exit 1
     touch ${WORKDIR}/_.built-videocore-library
-)
+}
 
 # $1: DESTDIR
-videocore_user_install ( ) (
+videocore_user_install ( ) {
     echo "TODO: Install videocore library"
     exit 1
-)
+}
