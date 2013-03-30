7b1a86dbebfc6df808d7d83a7ea0dc8983deb5ae
Tim Kientzle <kientzle@acm.org>
2013-03-30 15:48:58 -0700

    Implement a basic option framework, convert the AutoSize feature into the first proof-of-concept.

--- lib/base.sh
+++ lib/base.sh
@@ -14,3 +14,52 @@ load_config ( ) {
 	exit 1
     fi
 }
+
+option ( ) {
+    OPTION=$1
+    shift
+    OPTIONDIR=${TOPDIR}/option/${OPTION}
+    if [ ! -e ${OPTIONDIR}/setup.sh ]; then
+	echo "Cannot setup option $OPTION."
+	echo "No setup.sh in ${OPTIONDIR}."
+	exit 1
+    fi
+    . $OPTIONDIR/setup.sh "$@"
+    echo "Imported option $OPTION"
+}
+
+#
+# Options can hook any of the following points in the
+# image construction:
+#   * post-world:  After FreeBSD world is installed,
+#                  after board-specific customization,
+#                  but before the user customize hook.
+#
+# NOTE: These following hook functions are most definitely not
+# intended for user customizations, and should never be called
+# directly from config.sh.
+#
+# TODO: As new options are implemented, evaluate what hooks
+# are provided here and add new hooks as needed.
+#
+
+echo > ${WORKDIR}/options_post_installworld.sh
+
+# $@ - shell function and options
+add_option_post_installworld ( ) {
+    cat >>${WORKDIR}/options_post_installworld.sh <<EOF
+echo "Performing post-installworld $OPTION"
+OPTIONDIR=$OPTIONDIR $@
+EOF
+}
+
+# $1 - root of installed tree
+run_options_post_installworld ( ) {
+    cd $1
+    if /bin/sh -x ${WORKDIR}/options_post_installworld.sh; then
+	true # Everything went well.
+    else
+	echo "Post-installworld options failed."
+	exit 1
+    fi
+}
