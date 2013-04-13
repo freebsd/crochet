5200405473db84ceb4525475c5f0376a80a17676
Tim Kientzle <kientzle@acm.org>
2013-04-13 01:59:43 -0700

    Print out a more accurate suggestion for building xdev tools; thanks for skreuzer for the idea

--- lib/freebsd_xdev.sh
+++ lib/freebsd_xdev.sh
@@ -3,17 +3,28 @@ FREEBSD_XDEV_PREFIX=
 
 # freebsd_xdev_test: Verify that xdev tools exist.
 #
-# TODO: support armv6 here as well.
 freebsd_xdev_test ( ) {
-    FREEBSD_XDEV_PREFIX=${TARGET_ARCH}-freebsd-
+    XDEV_ARCH=${TARGET_ARCH}
+    case ${XDEV_ARCH} in
+	arm*) XDEV=arm
+	    ;;
+	mips*) XDEV=mips
+	    ;;
+	pc98) XDEV=i386
+	    ;;
+	powerpc*) XDEV=powerpc
+	    ;;
+	*) XDEV=${XDEV_ARCH}
+	    ;;
+    esac
+
+    FREEBSD_XDEV_PREFIX=${XDEV_ARCH}-freebsd-
     CC=${FREEBSD_XDEV_PREFIX}cc
-    # We need the cross-tools for arm, if they're not already built.
-    # This should work with arm.arm or arm.armv6 equally well.
     if [ -z `which ${CC}` ]; then
-	echo "Can't find FreeBSD xdev tools for ARM."
+	echo "Can't find appropriate FreeBSD xdev tools."
 	echo "If you have FreeBSD-CURRENT sources in /usr/src, you can build these with the following command:"
 	echo
-	echo "cd /usr/src && sudo make XDEV_ARCH=${TARGET_ARCH} xdev"
+	echo "cd /usr/src && sudo make XDEV=${XDEV} XDEV_ARCH=${XDEV_ARCH} xdev"
 	echo
 	echo "Run this script again after you have the xdev tools installed."
 	exit 1
@@ -27,5 +38,5 @@ freebsd_xdev_test ( ) {
 	echo "Please install a newer version of the xdev tools."
 	exit 1
     fi
-    echo "Found FreeBSD xdev tools for ARM"
+    echo "Found FreeBSD xdev tools for ${XDEV_ARCH}"
 }
