d90fbc95182ee70d3da59bcaa75e029ef0a7ffdf
Tim Kientzle <kientzle@acm.org>
2013-05-23 22:31:55 -0700

    Sketch the Parallels guest builder.

---
+++ board/GenericI386/option/Parallels-guest/setup.sh
@@ -0,0 +1,11 @@
+#
+# TODO: Fill this in; should be very similar to the VMWare-guest option.
+#
+# The Parallels VM works similarly:  The VM itself is
+# a directory with a machine description, a virtual
+# disk description, and the virtual disk itself.
+#
+# Details of the Parallels machine description:
+# http://www.parallels.com/fileadmin/parallels/documents/support/pdfm/Parallels_Desktop_Advanced_VM_Configuration.pdf
+#
+#
