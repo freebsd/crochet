8882fd7990dd82c6b87306728285175ed40e4faa
Tim Kientzle <kientzle@acm.org>
2013-05-25 17:18:01 -0700

    Fix customize hooks by only installing them after the user defines them.

--- lib/customize.sh
+++ lib/customize.sh
@@ -1,19 +1,29 @@
 #
 # Crochet defines a handful of standard shell functions
 # to support end-user customization.  These are never
-# used or overridden by board or option definitions.
+# defined or overridden by board or option definitions.
 #
 
-# Run end-user customization late in the phase.
-# Note that PHASE_BOOT_INSTALL gets cwd set to boot mountpoint automatically.
-
-# COMMENTED OUT: On systems that don't use a boot partition, this
-# breaks things.  I haven't figured out a really good workaround;
-# people might just have to use strategy_add directly if they want to
-# do any boot-partition customization.
-
 #customize_boot_partition ( ) { }
-#PRIORITY=200 strategy_add $PHASE_BOOT_INSTALL customize_boot_partition ${BOARD_BOOT_MOUNTPOINT}
+#customize_freebsd_partition ( ) { }
+#customize_post_unmount ( ) { }
+
+# If any of the above are actually defined, add them to the
+# strategy.  We deliberately add them with a late priority.
+#
+install_customize_hooks ( ) {
+    # If customize_boot_partition was defined, add it.
+    if command -v customize_boot_partition >/dev/null 2>&1; then
+	PRIORITY=200 strategy_add $PHASE_BOOT_INSTALL customize_boot_partition ${BOARD_BOOT_MOUNTPOINT}
+    fi
+    if command -v customize_freebsd_partition >/dev/null 2>&1; then
+	PRIORITY=200 strategy_add $PHASE_FREEBSD_USER_CUSTOMIZATION customize_freebsd_partition ${BOARD_FREEBSD_MOUNTPOINT}
+    fi
+    if command -v customize_post_unmount >/dev/null 2>&1; then
+	PRIORITY=200 strategy_add $PHASE_POST_UNMOUNT customize_post_unmount
+    fi
+}
+strategy_add $PHASE_POST_CONFIG install_customize_hooks
 
 # Copy overlay files early in the user customization phase.
 # Typically, people want to copy static files and then
@@ -25,14 +35,3 @@ customize_overlay_files ( ) {
     fi
 }
 PRIORITY=50 strategy_add $PHASE_FREEBSD_USER_CUSTOMIZATION customize_overlay_files
-
-# Run the shell hook late.  This means that functions
-# explicitly registered by users calling strategy_add
-# will run before this.
-# Note that PHASE_FREEBSD_* gets cwd set to freebsd mountpoint automatically.
-customize_freebsd_partition ( ) { }
-PRIORITY=200 strategy_add $PHASE_FREEBSD_USER_CUSTOMIZATION customize_freebsd_partition ${BOARD_FREEBSD_MOUNTPOINT}
-
-# Run end-user customization late in the phase.
-customize_post_unmount ( ) { }
-PRIORITY=200 strategy_add $PHASE_POST_UNMOUNT customize_post_unmount
