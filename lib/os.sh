5f0a4abd2d1495b4debf7a3d5cc721fdc8007734
Tom Everett <tom@khubla.com>
2013-10-02 17:18:59 -0600

    reduced some output

--- lib/os.sh
+++ lib/os.sh
@@ -3,7 +3,7 @@
 determine_os_version ( ) {
     OS_VERSION=`/usr/bin/grep "REVISION=" ${FREEBSD_SRC}/sys/conf/newvers.sh | awk 'BEGIN {FS="="} {print $2}' | /usr/bin/tr -d '"'`
     MAJOR_OS_VERSION=`echo $OS_VERSION | awk 'BEGIN {FS="."} {print $1}'`
-    echo "OS version is: $OS_VERSION"; 
+#    echo "OS version is: $OS_VERSION"; 
     echo "OS major version is: $MAJOR_OS_VERSION";
 }
 
