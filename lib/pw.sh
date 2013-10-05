81cf444074c36c24f10e9724e0a3b5ad473c3a13
Tom Everett <tom@khubla.com>
2013-10-05 10:44:10 -0600

    users need home directories

--- lib/pw.sh
+++ lib/pw.sh
@@ -6,6 +6,7 @@ pw_create_crochet_account ( ) {
 
 pw_create_account ( ) {
 #    echo "${BOARD_FREEBSD_MOUNTPOINT}/etc/"
-    echo "Adding user $1 with password $1"
-    $PW useradd -n $1 -s /bin/csh -g wheel -w yes -V ${BOARD_FREEBSD_MOUNTPOINT}/etc/   
+    echo "Adding user $1 with password $1" 
+    mkdir -p ${BOARD_FREEBSD_MOUNTPOINT}/usr/home/$1
+    $PW useradd -n $1 -s /bin/csh -g wheel -w yes -V ${BOARD_FREEBSD_MOUNTPOINT}/etc/ -d /usr/home/$1
 }
