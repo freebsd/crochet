bd8b0a4476b8cd3e9e4e9d846c0bccaa10c74602
Tim Kientzle <kientzle@acm.org>
2013-10-06 11:01:14 -0700

    Clean up the code just a bit.

--- option/GzipImage/setup.sh
+++ option/GzipImage/setup.sh
@@ -1,18 +1,13 @@
 
 #
-# enable gzipping of images
+# Gzip the final image
 #
-GZIPIMAGE=$1
-
-# compress the image, since it's easier to SCP around this way
 gzip_image() {
-    if [ -n "${GZIPIMAGE}" ]; then
-        echo "Compressing image"
-        if [ -f $IMG.gz ]; then 
-           rm $IMG.gz
-        fi
-        gzip -k $IMG
+    echo "Compressing image"
+    if [ -f $IMG.gz ]; then
+       rm $IMG.gz
     fi
+    gzip -k $IMG
 }
 strategy_add $PHASE_POST_UNMOUNT gzip_image
 
