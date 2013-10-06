be9c842bd040bcdb0cf61247ef3fd20bb13b5d5a
Tim Kientzle <kientzle@acm.org>
2013-10-06 11:06:27 -0700

    Allow "option User" to be specified more than once.

--- option/User/setup.sh
+++ option/User/setup.sh
@@ -1,15 +1,14 @@
 
 PW=/usr/sbin/pw
-LOGINUSER=$1
 
 pw_create_account ( ) {
     if [ -n "${GZIPIMAGE}" ]; then
-        echo "Adding user $LOGINUSER with password $LOGINUSER" 
-        mkdir -p ${BOARD_FREEBSD_MOUNTPOINT}/usr/home/$LOGINUSER
-        $PW useradd -n $LOGINUSER -s /bin/csh -g wheel -w yes -V ${BOARD_FREEBSD_MOUNTPOINT}/etc/ -d /usr/home/$LOGINUSER
+        echo "Adding user $1 with password $1"
+        mkdir -p ${BOARD_FREEBSD_MOUNTPOINT}/usr/home/$1
+        $PW useradd -n $1 -s /bin/csh -g wheel -w yes -V ${BOARD_FREEBSD_MOUNTPOINT}/etc/ -d /usr/home/$1
     fi
 }
 
-# add a "crochet" account
-strategy_add $PHASE_FREEBSD_BOARD_INSTALL pw_create_account
+# Add the specified account.
+strategy_add $PHASE_FREEBSD_BOARD_INSTALL pw_create_account $1
 
