a29dac714332043c67910f46a2c50acdc67f719d
Tom Everett <tom@khubla.com>
2013-12-05 19:22:31 -0700

    patches are applied

--- board/WandboardQuad/setup.sh
+++ board/WandboardQuad/setup.sh
@@ -4,6 +4,22 @@ IMAGE_SIZE=$((1024 * 1000 * 1000))
 WANDBOARD_UBOOT_SRC=${TOPDIR}/u-boot-2013.10
 
 #
+# 2 partitions, a FAT one for the boot and a UFS one
+#
+wandboard_partition_image ( ) {
+    disk_partition_mbr
+    disk_fat_create 2m
+    disk_ufs_create
+}
+strategy_add $PHASE_PARTITION_LWW wandboard_partition_image
+
+wandboard_partition_image_mount_partitions ( ) {
+    disk_fat_mount ${BOARD_BOOT_MOUNTPOINT}
+    disk_ufs_mount ${BOARD_FREEBSD_MOUNTPOINT}
+}
+strategy_add $PHASE_MOUNT_LWW wandboard_partition_image_mount_partitions
+
+#
 # Wandboard uses U-Boot.
 #
 # patches come from here https://raw.github.com/eewiki/u-boot-patches/master/v2013.10/0001-wandboard-uEnv.txt-bootz-n-fixes.patch
@@ -15,36 +31,12 @@ wandboard_check_uboot ( ) {
             "$WANDBOARD_UBOOT_SRC/board/ti/am335x/Makefile" \
             "ftp ftp://ftp.denx.de/pub/u-boot/u-boot-2013.10.tar.bz2" \
             "tar xf u-boot-2013.10.tar.bz2"
-        strategy_add $PHASE_BUILD_OTHER uboot_patch ${WANDBOARD_UBOOT_SRC} ${BOARDDIR}/files/uboot_*.patch
+        strategy_add $PHASE_BUILD_OTHER uboot_patch ${WANDBOARD_UBOOT_SRC} ${BOARDDIR}/files/*.patch
         strategy_add $PHASE_BUILD_OTHER uboot_configure $WANDBOARD_UBOOT_SRC wandboard_quad_config
         strategy_add $PHASE_BUILD_OTHER uboot_build $WANDBOARD_UBOOT_SRC
 }
 strategy_add $PHASE_CHECK wandboard_check_uboot
 
-# create a MBR disk with a single UFS partition
-# based on instructions here: http://www.wonkity.com/~wblock/docs/html/disksetup.html
-#
-wandboardquad_partition_image ( ) {
-        BOOTFILES=${OBJFILES}sys/boot/i386
-        echo "Boot files are at: "${BOOTFILES}
-
-        # basic setup
-        disk_partition_mbr
-        disk_ufs_create
-
-        # boot loader
-        echo "Installing bootblocks"
-        gpart bootcode -b ${BOOTFILES}/mbr/mbr ${DISK_MD} || exit 1
-        gpart set -a active -i 1 ${DISK_MD} || exit 1
-        bsdlabel -B -b ${BOOTFILES}/boot2/boot ${DISK_UFS_PARTITION} || exit 1
-
-        #show the disk
-        gpart show ${DISK_MD}
-}
-
-strategy_add $PHASE_PARTITION_LWW wandboardquad_partition_image
-
-# Kernel installs in UFS partition
 strategy_add $PHASE_FREEBSD_BOARD_INSTALL freebsd_installkernel .
 
 
