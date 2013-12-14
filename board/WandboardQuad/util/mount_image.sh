c5b0a32cc92f70066bf0470845267bf115bc3c0d
Tom Everett <tom@khubla.com>
2013-12-13 17:25:17 -0700

    fixed mount scripts

--- board/WandboardQuad/util/mount_image.sh
+++ board/WandboardQuad/util/mount_image.sh
@@ -1,4 +1,5 @@
-mdconfig -a -t vnode -f ../../../work/FreeBSD-arm-11.0-WANDBOARD-QUAD.img -u 0
+IMAGE=`ls ../../../work/*.img`
+mdconfig -a -t vnode -f ${IMAGE} -u 0
 mkdir -p /tmp/imagemount_root
 mkdir -p /tmp/imagemount_freebsd
 mount_msdosfs /dev/md0s1 /tmp/imagemount_root
