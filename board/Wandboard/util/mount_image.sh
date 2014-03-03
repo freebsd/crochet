5e3c95c265f04375ee2f3716821dd590af1842d7
Tom Everett <tom@khubla.com>
2014-03-02 18:41:53 -0700

    updates to allow putting dtb file on UFS partition. uEnv.txt updated to pass the name of the dtb file to ubldr

--- board/Wandboard/util/mount_image.sh
+++
@@ -1,8 +0,0 @@
-IMAGE=`ls ../../../work/*.img`
-mdconfig -a -t vnode -f ${IMAGE} -u 0
-mkdir -p /tmp/imagemount_root
-mkdir -p /tmp/imagemount_freebsd
-mount_msdosfs /dev/md0s1 /tmp/imagemount_root
-mount /dev/md0s2a /tmp/imagemount_freebsd
-
-
--- board/Wandboard/util/unmount_image.sh
+++
@@ -1,4 +0,0 @@
-umount /tmp/imagemount_root
-umount /tmp/imagemount_freebsd
-mdconfig -d -u 0
-
