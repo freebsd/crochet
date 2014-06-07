179d8d5cd1216b66798c8b3a763c97aa7c24f5ae
Tim Kientzle <kientzle@acm.org>
2014-06-07 14:36:02 -0700

    Paste a zero-length string to ensure back-tick substitution doesn't disappear

--- board/BeagleBone/setup.sh
+++ board/BeagleBone/setup.sh
@@ -29,7 +29,7 @@ uboot_eabi_port_location ( ) {
 beaglebone_check_uboot ( ) {
     if [ -n "${BEAGLEBONE_UBOOT}" ]; then
 	echo "Using U-Boot from location: ${BEAGLEBONE_UBOOT}"
-    elif [ -n `uboot_eabi_port_version` ]; then
+    elif [ -n ""`uboot_eabi_port_version` ]; then
         echo "Using U-Boot from port: "`uboot_eabi_port_version`
 	BEAGLEBONE_UBOOT=`uboot_eabi_port_location`/share/u-boot/beaglebone-eabi/
     elif [ -n "${BEAGLEBONE_UBOOT_SRC}" ]; then
