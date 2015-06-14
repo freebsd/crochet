103de327f3a093213fe83a54dba229850ac21320
Tim Kientzle <kientzle@acm.org>
2015-06-14 14:35:34 -0700

    Put -V option before the subcommand, as required by recent changes to 'pw'

--- option/User/setup.sh
+++ option/User/setup.sh
@@ -2,15 +2,16 @@
 PW=/usr/sbin/pw
 CHOWN=/usr/sbin/chown
 AWK=/usr/bin/awk
+HOME_DIR=/usr/home
 
 pw_create_account ( ) {
     echo "Adding user $1 with password $1"
-    mkdir -p ${BOARD_FREEBSD_MOUNTPOINT}/usr/home/$1
-    $PW useradd -n $1 -s /bin/csh -g wheel -w yes -V ${BOARD_FREEBSD_MOUNTPOINT}/etc/ -d /usr/home/$1
+    mkdir -p ${BOARD_FREEBSD_MOUNTPOINT}${HOME_DIR}/$1
+    $PW -V ${BOARD_FREEBSD_MOUNTPOINT}/etc/ useradd -n $1 -s /bin/csh -g wheel -w yes -d ${HOME_DIR}/$1
 
     # Fetch the uid and gid from the target and use the numeric ids to set the ownership
-    UGID=`$PW usershow $1 -V ${BOARD_FREEBSD_MOUNTPOINT}/etc/ | $AWK -F: '{ print $3 ":" $4 }'`
-    $CHOWN $UGID ${BOARD_FREEBSD_MOUNTPOINT}/usr/home/$1
+    UGID=`$PW -V ${BOARD_FREEBSD_MOUNTPOINT}/etc/ usershow $1 | $AWK -F: '{ print $3 ":" $4 }'`
+    $CHOWN $UGID ${BOARD_FREEBSD_MOUNTPOINT}${HOME_DIR}/$1
 }
 
 # Add the specified account.
