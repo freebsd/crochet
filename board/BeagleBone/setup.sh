8e48881c34171d1e60529e864487c123991c4fe4
Tim Kientzle <kientzle@acm.org>
2015-07-17 21:06:53 -0700

    Switch BeagleBone board config to use U-Boot from ports.

--- board/BeagleBone/setup.sh
+++ board/BeagleBone/setup.sh
@@ -1,8 +1,14 @@
 KERNCONF=BEAGLEBONE
-BEAGLEBONE_UBOOT=
-BEAGLEBONE_UBOOT_SRC=${TOPDIR}/u-boot-2014.04
+BEAGLEBONE_UBOOT_PORT="u-boot-beaglebone"
+BEAGLEBONE_UBOOT_BIN="u-boot.img"
+BEAGLEBONE_UBOOT_PATH="/usr/local/share/u-boot/${BEAGLEBONE_UBOOT_PORT}"
 IMAGE_SIZE=$((1000 * 1000 * 1000))
 TARGET_ARCH=armv6
+
+beaglebone_check_uboot ( ) {
+    uboot_port_test ${BEAGLEBONE_UBOOT_PORT} ${BEAGLEBONE_UBOOT_BIN}
+}
+strategy_add $PHASE_CHECK beaglebone_check_uboot
 
 #
 # BeagleBone requires a FAT partition to hold the boot loader bits.
@@ -14,50 +20,11 @@ beaglebone_partition_image ( ) {
 }
 strategy_add $PHASE_PARTITION_LWW beaglebone_partition_image
 
-#
-# BeagleBone uses U-Boot.
-#
-beaglebone_check_uboot ( ) {
-    if [ -n "${BEAGLEBONE_UBOOT}" ]; then
-	echo "Using U-Boot from location: ${BEAGLEBONE_UBOOT}"
-    elif [ -n "${BEAGLEBONE_UBOOT_SRC}" ]; then
-        # Crochet needs to build U-Boot.
-
-	uboot_set_patch_version ${BEAGLEBONE_UBOOT_SRC} ${BEAGLEBONE_UBOOT_PATCH_VERSION}
-
-        uboot_test \
-            BEAGLEBONE_UBOOT_SRC \
-            "$BEAGLEBONE_UBOOT_SRC/board/ti/am335x/Makefile"
-        strategy_add $PHASE_BUILD_OTHER uboot_patch ${BEAGLEBONE_UBOOT_SRC} `uboot_patch_files`
-        strategy_add $PHASE_BUILD_OTHER uboot_configure $BEAGLEBONE_UBOOT_SRC am335x_boneblack_config
-        strategy_add $PHASE_BUILD_OTHER uboot_build $BEAGLEBONE_UBOOT_SRC
-    else
-	echo
-	echo "Don't know where to find U-Boot."
-	echo "Please set \$BEAGLEBONE_UBOOT_SRC"
-	echo
-	exit 1
-    fi
-
-}
-strategy_add $PHASE_CHECK beaglebone_check_uboot
-
-
 beaglebone_uboot_install ( ) {
-    if [ -n "${BEAGLEBONE_UBOOT}" ]; then
-        echo "Installing U-Boot from : ${BEAGLEBONE_UBOOT}"
-	cp ${BEAGLEBONE_UBOOT}/MLO .
-	cp ${BEAGLEBONE_UBOOT}/bb-uboot.img .
-	cp ${BEAGLEBONE_UBOOT}/bb-uenv.txt .
-    else
-        echo "Installing U-Boot onto the FAT partition"
-        # Note that all of the BeagleBone boot files
-        # start with 'BB' now (except for MLO, which can't
-        # be renamed because it's loaded by the ROM).
-        cp ${BEAGLEBONE_UBOOT_SRC}/MLO .
-        cp ${BEAGLEBONE_UBOOT_SRC}/u-boot.img bb-uboot.img
-        cp ${BOARDDIR}/files/uEnv.txt bb-uEnv.txt
-    fi
+    echo "Installing U-Boot from: ${BEAGLEBONE_UBOOT_PATH}"
+    cp ${BEAGLEBONE_UBOOT_PATH}/MLO .
+    cp ${BEAGLEBONE_UBOOT_PATH}/u-boot.img .
+    touch uEnv.txt
     freebsd_install_fdt beaglebone.dts bbone.dts
     freebsd_install_fdt beaglebone.dts bbone.dtb
     freebsd_install_fdt beaglebone-black.dts bboneblk.dts
@@ -65,11 +32,9 @@ beaglebone_uboot_install ( ) {
 }
 strategy_add $PHASE_BOOT_INSTALL beaglebone_uboot_install
 
-# TODO: Try changing ubldr to a PIC binary instead of ELF, so we don't
-# have to compile it separately for every different load address.
-#
+# Build and install a suitable ubldr
 strategy_add $PHASE_BUILD_OTHER freebsd_ubldr_build UBLDR_LOADADDR=0x88000000
-strategy_add $PHASE_BOOT_INSTALL freebsd_ubldr_copy_ubldr bbubldr
+strategy_add $PHASE_BOOT_INSTALL freebsd_ubldr_copy_ubldr ubldr
 
 # BeagleBone puts the kernel on the FreeBSD UFS partition.
 strategy_add $PHASE_FREEBSD_BOARD_INSTALL board_default_installkernel .
