9348c9fe9493454a4f6dd555e10f509685a9c8fc
Josua Mayer <josua.mayer97@gmail.com>
2018-05-13 13:43:30 +0000

    introduce support for the SolidRun clearfog
    Initial support for the SolidRun Clearfog laned in FreeBSD with r319914.
    The board has acceptable support in mainline U-Boot, as well as a
    vendor fork based on 2018.01.
    For now an MBR partition table is used, with a fat partition to load
    DTB and ubldr from, reserved space covering the first 1M of the disk
    to make sure installing u-boot later doesn't destroy the boot partition.
    
    Signed-off-by: Josua Mayer <josua.mayer97@gmail.com>

---
+++ board/Clearfog/setup.sh
@@ -0,0 +1,51 @@
+KERNCONF=GENERIC
+TARGET_ARCH=armv7
+IMAGE_SIZE=$((2 * 1000 * 1000 * 1000))
+
+# check for mkimage utility
+clearfog_check_uboot-tools ( ) {
+    if [ -z `which mkimage` ]; then
+        echo "Clearfog Boot-Script build requires 'mkimage'"
+        echo "Please install sysutils/u-boot-tools and re-run this script."
+        exit 1
+    fi
+}
+strategy_add $PHASE_CHECK clearfog_check_uboot-tools
+
+# build ubldr
+strategy_add $PHASE_BUILD_OTHER freebsd_ubldr_build
+
+# partition disk
+# 1: reserved space for u-boot (first 1M into the device)
+# 2: boot
+# 3: FreeBSD
+clearfog_disk_reserved_create_unaligned ( ) {
+    _DISK_RESERVED_SLICE=`gpart add -s $1 -t '!218' ${DISK_MD} | sed -e 's/ .*//'`
+    disk_created_new RESERVED ${_DISK_RESERVED_SLICE}
+}
+clearfog_partition_image ( ) {
+    disk_partition_mbr
+    clearfog_disk_reserved_create_unaligned 1985
+    disk_fat_create 2m
+    disk_ufs_create
+    disk_ufs_label 1 rootfs
+}
+strategy_add $PHASE_PARTITION_LWW clearfog_partition_image
+
+# install boot files
+clearfog_install_boot-files ( ) {
+    freebsd_ubldr_copy_ubldr ${BOARD_BOOT_MOUNTPOINT}
+    mkimage -A arm -O FreeBSD -T script -a 0 -e 0 -d "$BOARDDIR/files/boot.txt" "boot.scr" > ${WORKDIR}/_.mkimage.log
+    freebsd_install_fdt ../gnu/dts/arm/armada-388-clearfog-base.dts armada-388-clearfog-base.dtb
+    freebsd_install_fdt ../gnu/dts/arm/armada-388-clearfog-pro.dts armada-388-clearfog-pro.dtb
+}
+strategy_add $PHASE_BOOT_INSTALL clearfog_install_boot-files
+
+# install kernel on rootfs
+strategy_add $PHASE_FREEBSD_BOARD_INSTALL board_default_installkernel .
+
+# install ubldr help and configuration to rootfs
+strategy_add $PHASE_FREEBSD_BOARD_INSTALL freebsd_ubldr_copy boot
+
+# create mount-point for boot partition
+strategy_add $PHASE_FREEBSD_BOARD_INSTALL mkdir -p boot/msdos
