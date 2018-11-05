879fd337dac18716ac5329b6e7419837c5603a73
Brad Davis <brd@FreeBSD.org>
2018-11-05 08:22:19 -0700

    Refactor to simplify the install and add support for building 32-bit images for the RPi3.

--- board/RaspberryPi3/setup.sh
+++ board/RaspberryPi3/setup.sh
@@ -1,3 +1,7 @@
+TARGET_ARCH=aarch64
+TARGET=aarch64
+#TARGET=arm
+#TARGET_ARCH=armv7
 KERNCONF=GENERIC
 RPI3_UBOOT_PORT="u-boot-rpi3"
 RPI3_UBOOT_BIN="u-boot.bin"
@@ -5,9 +9,13 @@ RPI3_UBOOT_PATH="${SHARE_PATH}/u-boot/${RPI3_UBOOT_PORT}"
 RPI_FIRMWARE_PORT="rpi-firmware"
 RPI_FIRMWARE_BIN="bootcode.bin"
 RPI_FIRMWARE_PATH="${SHARE_PATH}/${RPI_FIRMWARE_PORT}"
+RPI_FIRMWARE_FILES="bootcode.bin \
+    fixup.dat fixup_cd.dat fixup_db.dat fixup_x.dat \
+    start.elf start_cd.elf start_db.elf start_x.elf"
+if [ ${TARGET} == "aarch64" ]; then
+    RPI_FIRMWARE_FILES="${RPI_FIRMWARE_FILES} armstub8.bin"
+fi
 IMAGE_SIZE=$((3 * 1000 * 1000 * 1000))
-TARGET_ARCH=aarch64
-TARGET=aarch64
 
 # Not used - just in case someone wants to use a manual ubldr.  Obtained
 # from 'printenv' in boot0: kernel_addr_r=0x42000000
@@ -35,25 +43,36 @@ rpi3_partition_image ( ) {
 strategy_add $PHASE_PARTITION_LWW rpi3_partition_image
 
 raspberry_pi_populate_boot_partition ( ) {
-    echo bootaa64 > startup.nsh
     mkdir -p EFI/BOOT
-    cp -R ${RPI_FIRMWARE_PATH}/ .
+    for file in ${RPI_FIRMWARE_FILES}; do
+        cp ${RPI_FIRMWARE_PATH}/${file} .
+    done
     cp ${UBOOT_PATH}/u-boot.bin .
     cp ${UBOOT_PATH}/README .
 
     # Populate config.txt
-    echo "arm_control=0x200" > config.txt
-    echo "dtparam=audio=on,i2c_arm=on,spi=on" >> config.txt
-    echo "dtoverlay=mmc" >> config.txt
-    echo "dtoverlay=pi3-disable-bt" >> config.txt
-    echo "device_tree_address=0x4000" >> config.txt
-    echo "kernel=u-boot.bin" >> config.txt
+    if [ ${TARGET} == "aarch64" ]; then
+        cp ${RPI_FIRMWARE_PATH}/config_rpi3.txt config.txt
+    else
+        cp ${RPI_FIRMWARE_PATH}/config.txt config.txt
+        echo "dtoverlay=pi3-disable-bt" >> config.txt
+    fi
+
+    # Copy in overlays
+    cp -R ${RPI_FIRMWARE_PATH}/overlays .
+
+    # Populate DTB
+    cp ${RPI_FIRMWARE_PATH}/bcm2710-rpi-3-b.dtb .
 }
 strategy_add $PHASE_BOOT_INSTALL raspberry_pi_populate_boot_partition
 
 # Build & install loader.efi.
 strategy_add $PHASE_BUILD_OTHER freebsd_loader_efi_build
-strategy_add $PHASE_BOOT_INSTALL freebsd_loader_efi_copy EFI/BOOT/bootaa64.efi
+if [ ${TARGET} == "aarch64" ]; then
+    strategy_add $PHASE_BOOT_INSTALL freebsd_loader_efi_copy EFI/BOOT/bootaa64.efi
+else
+    strategy_add $PHASE_BOOT_INSTALL freebsd_loader_efi_copy EFI/BOOT/bootarm.efi
+fi
 
 # RPi3 puts the kernel on the FreeBSD UFS partition.
 strategy_add $PHASE_FREEBSD_BOARD_INSTALL board_default_installkernel .
