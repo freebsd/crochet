fa36fee32ffd26bf3a68a858a37a690365c270bf
Tom Everett <tom@khubla.com>
2024-07-02 08:32:02 -0600

    vars

--- ci.sh
+++ ci.sh
@@ -1,11 +1,16 @@
 #! /bin/sh
 
+# vars
+SOURCE_DIR=/crochet/src
+SOURCE_URL=https://github.com/freebsd/freebsd-src.git
+SOURCE_BRANCH=stable/13
+
 # install git
 echo "installing git"
 pkg install -y git
 
 # clone FreeBSD source
-echo "cloning FreeBSD"
+echo "cloning FreeBSD from $SOURCE_BRANCH branch $SOURCE_BRANCH into $SOURCE_DIR"
 /bin/rm -rf /crochet/src
-git clone https://github.com/freebsd/freebsd-src.git -b stable/13 /crochet/src 
+git clone $SOURCE_URL -b $SOURCE_BRANCH /crochet/src 
 
